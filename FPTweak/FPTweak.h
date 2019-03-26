efcbcea0d3fca170d460eb545a6fd2dd1e8b7fdc
lemon4ex <netchen@vip.qq.com>
2019-03-27 01:03:34 +0800

    Initial Commit

---
+++ FPTweak/FPTweak.h
@@ -0,0 +1,9 @@
+//
+//  FPTweak.h
+//  FPTweak
+//
+//  Created by h4ck on 2019/3/27.
+//  Copyright (c) 2019年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ FPTweak/FPTweak.m
@@ -0,0 +1,15 @@
+//
+//  FPTweak.m
+//  FPTweak
+//
+//  Created by h4ck on 2019/3/27.
+//  Copyright (c) 2019年 __MyCompanyName__. All rights reserved.
+//
+
+#import "FPTweak.h"
+#import <CaptainHook/CaptainHook.h>
+
+CHConstructor // code block that runs immediately upon load
+{
+    NSLog(@"++++++++ FPTweak loaded ++++++++");
+}
